727235b268ed966a061cfd8b731222653d86b828
Sue-Holding <sue.holding55@gmail.com>
2026-03-20 20:55:43 +0100

    added events in constants and event card on page - homepage with an explore events btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/exploreBtn";
+import { events } from "@/lib/constants";
+
 const Page = () => {
   return (
     <section>
       <h1 className="text-center">The best event page ever</h1>
       <p className="text-center mt-5">Hackathans, Meetups and Conferences all in one place</p>
+    
+    <ExploreBtn />
+
+    <div className="mt-20 space-y.7">
+      <h3>Featured Events
+      </h3>
+
+      <ul className="events">
+        {events.map((event) => (
+          <li key={event.title}>
+            <EventCard { ...event} />
+          </li>
+        ))}
+      </ul>
+    </div>
     </section>
     
   )
